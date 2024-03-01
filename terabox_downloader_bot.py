ebe3a08d38f008e90b6778e0d704e173004b8eea
MOGAMBO <93328169+MOGAMBO-XD@users.noreply.github.com>
2024-03-01 23:30:15 +0530

    Update terabox_downloader_bot.py

--- terabox_downloader_bot.py
+++ terabox_downloader_bot.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, Update
 import requests
 import os
 
